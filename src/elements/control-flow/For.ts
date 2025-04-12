0d42ce45fb55d6903c0f4d72d9acb750be3dd90e
Jonathan Lange <jonathanlange@web.de>
2025-04-12 19:53:37 +0200

    remove need for parent element with ph-for template

--- src/elements/control-flow/For.ts
+++ src/elements/control-flow/For.ts
@@ -5,30 +5,32 @@ import { allElementsTreeWalker, createContextComputed, phSlotSyntax } from '@uti
 import { Directives, processAttributes, processAttributesForChildrenElements, renameShortcutAttributesInTemplate } from '@utils/Attributes'
 
 const createElement = (content: HTMLElement, listname: string, itemName: string, index: number, indexName?: string) => {
-        const el = content
-
-        const replaceRegExp = (what) => new RegExp(`([^a-zA-Z0-9$_]${what}[^a-zA-Z0-9$_:])`, 'g')
-        const replaceItemName = (x: string) => {
-                const result = x.replace(replaceRegExp(itemName), (s) => `${s[0]}${listname}[${index}]?.valueOf()${s.at(-1)}`)
-                if (indexName) return result.replace(replaceRegExp(indexName), (s) => `${s[0]}${index}${s.at(-1)}`)
-                return result
-        }
-
-        const replaced = phSlotSyntax(el.innerHTML, replaceItemName)
-        el.innerHTML = replaced
-        const replaceAttributeValues = (element: Element): void => {
-                for (const attribute of element.getAttributeNames()) {
-                        if (Object.keys(Directives).some((it) => attribute.startsWith(it))) {
-                                element.setAttribute(attribute, replaceItemName(` ${element.getAttribute(attribute)} `))
+        const //
+                el = content,
+                replaceRegExp = (what) => new RegExp(`([^a-zA-Z0-9$_]${what}[^a-zA-Z0-9$_:])`, 'g'),
+                replaceItemName = (x: string) => {
+                        const result = x.replace(replaceRegExp(itemName), (s) => `${s[0]}${listname}[${index}]?.valueOf()${s.at(-1)}`)
+                        if (indexName) return result.replace(replaceRegExp(indexName), (s) => `${s[0]}${index}${s.at(-1)}`)
+                        return result
+                },
+                replaceAttributeValues = (element: Element): void => {
+                        for (const attribute of element.getAttributeNames()) {
+                                if (Object.keys(Directives).some((it) => attribute.startsWith(it))) {
+                                        element.setAttribute(attribute, replaceItemName(` ${element.getAttribute(attribute)} `))
+                                }
                         }
-                }
-        }
+                },
+                replaced = phSlotSyntax(el.innerHTML, replaceItemName)
+
+        el.innerHTML = replaced
         replaceAttributeValues(el)
+
         const tw = allElementsTreeWalker(el)
         while (tw.nextNode()) {
-                const currentNode = tw.currentNode as Element
+                const //
+                        currentNode = tw.currentNode as Element,
+                        tagName = currentNode.tagName
                 replaceAttributeValues(currentNode)
-                const tagName = currentNode.tagName
                 if (currentNode.hasAttribute('value') && (tagName === 'PH-IF' || tagName === 'PH-EITHER' || tagName === 'PH-SHOW' || tagName === 'PH-SLOT')) {
                         currentNode.setAttribute('value', replaceItemName(` ${currentNode.getAttribute('value')} `))
                 }
@@ -43,18 +45,25 @@ export class PhFor extends Base {
                 if (templateElement?.tagName === 'TEMPLATE') {
                         templateElement.setAttribute('each', this.getAttribute('each'))
                         PhFor.fromTemplate(templateElement as HTMLTemplateElement)
-                } else throw Error('no template element provided')
+                } else {
+                        throw Error('no template element provided')
+                }
         }
 
         static fromTemplate(template: HTMLTemplateElement) {
                 renameShortcutAttributesInTemplate(template.content)
-                const eachAttribute = template.getAttribute('each')
-                const [definition, collection] = eachAttribute?.split(' in ') ?? []
-                const [itemName, indexName] = definition.split(',').map((it) => it.trim())
 
-                const root = template.getRootNode() as ShadowRoot
-                const context = (root.host ?? root)[CONTEXT]
-                const parentElement = template.parentElement
+                template.before(document.createComment('ph-for begin'))
+                template.after(document.createComment('ph-for end'))
+
+                const //
+                        eachAttribute = template.getAttribute('each'),
+                        beginComment = template.previousSibling,
+                        endComment = template.nextSibling,
+                        root = template.getRootNode() as ShadowRoot,
+                        context = (root.host ?? root)[CONTEXT],
+                        [definition, collection] = eachAttribute?.split(' in ') ?? [],
+                        [itemName, indexName] = definition.split(',').map((it) => it.trim())
 
                 if (context && itemName && collection && template) {
                         const computed = createContextComputed<Array<unknown>>(context, collection)
@@ -66,23 +75,28 @@ export class PhFor extends Base {
                                                 // add elements
                                                 const startingIndex = oldLength
                                                 computed.value.slice(startingIndex).forEach((_item, index) => {
-                                                        const content = template.content.firstElementChild.cloneNode(true) as HTMLElement
-                                                        const created = createElement(content, collection, itemName, index + startingIndex, indexName)
-                                                        const disposes = processAttributesForChildrenElements(created, context)
+                                                        const //
+                                                                content = template.content.firstElementChild.cloneNode(true) as HTMLElement,
+                                                                created = createElement(content, collection, itemName, index + startingIndex, indexName),
+                                                                disposes = processAttributesForChildrenElements(created, context)
                                                         disposes.push(...processAttributes(created, context))
                                                         created.disposes = disposes
-                                                        parentElement.append(created)
+                                                        endComment.before(created)
                                                 })
                                         } else {
                                                 // remove elements
-                                                const removingElements = Array.from(parentElement.children).slice(currentLength - oldLength)
-                                                for (const child of removingElements) {
-                                                        for (const d of child.disposes) {
-                                                                d()
-                                                        }
-                                                        child.remove()
+                                                const removingElements: HTMLElement[] = []
+                                                let currentNode = beginComment.nextSibling
+                                                let i = 0
+                                                while (currentNode && currentNode !== endComment) {
+                                                        if (i >= currentLength) removingElements.push(currentNode as HTMLElement)
+                                                        i++
+                                                        currentNode = currentNode.nextSibling
                                                 }
-                                                parentElement.remove
+                                                for (const child of removingElements) {
+                                                        child.disposes?.forEach((d) => d())
+                                                        child.remove?.()
+                                                }
                                         }
                                         oldLength = computed.value.length
                                 }
--- test/for.test.ts
+++ test/for.test.ts
@@ -1,5 +1,5 @@
 import test, { expect } from '@playwright/test'
-import { createContent } from './utils'
+import { createContent, debug } from './utils'
 
 test('test ph-for element', async ({ page }) => {
         await page.setContent(
@@ -49,7 +49,7 @@ test('test for template element with parent element', async ({ page }) => {
         await expect(page.locator('#id-2')).toHaveText('3')
 })
 
-test.skip('test for template element without parent element', async ({ page }) => {
+test('test for template element without parent element', async ({ page }) => {
         await page.setContent(
                 createContent(/*html*/ `
                 <ph-component>
@@ -69,4 +69,53 @@ test.skip('test for template element without parent element', async ({ page }) =
         await expect(page.locator('#id-0')).toHaveText('1')
         await expect(page.locator('#id-1')).toHaveText('2')
         await expect(page.locator('#id-2')).toHaveText('3')
+})
+
+test('for element should adapt to length changes', async ({ page }) => {
+        await page.setContent(
+                createContent(/*html*/ `
+                <ph-component>
+                        <template tag="test-div">
+                                <ph-signal name="items" value="[1, 2, 3]"></ph-signal>
+
+                                <template each="item, idx in items">
+                                        <div :id="'id' + '-' + idx">{{ item }}</div>
+                                </template>
+
+                                <button @click="items = [...items, items.length + 1]">+</button>
+                                <button @click="items = items.slice(0, -1)">-</button>
+                        </template>
+                </ph-component>
+
+                <test-div></test-div>
+                `),
+                { waitUntil: 'domcontentloaded' },
+        )
+        await expect(page.locator('#id-0')).toHaveText('1')
+        await expect(page.locator('#id-1')).toHaveText('2')
+        await expect(page.locator('#id-2')).toHaveText('3')
+
+        const addButton = page.locator('button').nth(0)
+        await addButton.click()
+        await expect(page.locator('#id-3')).toHaveText('4')
+
+        const removeButton = page.locator('button').nth(1)
+
+        await removeButton.click()
+        await expect(page.locator('#id-0')).toHaveText('1')
+        await expect(page.locator('#id-1')).toHaveText('2')
+        await expect(page.locator('#id-2')).toHaveText('3')
+        await expect(page.locator('#id-3')).not.toBeAttached()
+
+        await removeButton.click()
+        await expect(page.locator('#id-0')).toHaveText('1')
+        await expect(page.locator('#id-1')).toHaveText('2')
+        await expect(page.locator('#id-2')).not.toBeAttached()
+        await expect(page.locator('#id-3')).not.toBeAttached()
+
+        await removeButton.click()
+        await expect(page.locator('#id-0')).toHaveText('1')
+        await expect(page.locator('#id-1')).not.toBeAttached()
+        await expect(page.locator('#id-2')).not.toBeAttached()
+        await expect(page.locator('#id-3')).not.toBeAttached()
 })
